d66d47fde40f812ba1402dec536089d7d7b2d438
Javier Eduardo Labat <javier.labat@sdc.com.ar>
2023-01-06 17:29:02 -0300

    mockService

--- reactTestingLibrary/src/App.test.js
+++ reactTestingLibrary/src/App.test.js
@@ -4,8 +4,6 @@ import { getWordMeaning } from './services/DictionaryServiceImpl'
 import App from "./App";
 
 
-//jest.mock('axios');
-
 beforeEach( () =>{
     //Renderizo App
     render(<App />);
@@ -37,35 +35,18 @@ test('Testeo insercion de input en placeHolder y llamado del servicio, simulando
     fireEvent.change(inputPlaceholder, {target: {value: 'defnition'}});
     fireEvent.click(submitButton);
 });
-
+/*
 test('Testeo llamado del servicio Real (No recomendado)', async () =>{
-    //Busco en la pantalla un objeto en el dom que contenga el Placeholder en cuestion, en este caso es el input para insertar la keySearch
+    //Llamo al servicio con el keySearch defnition que se que no trae resultados
+    const responseService = await getWordMeaning('defnition');
+    expect(responseService).toBe('Result not found, try other word');
+});
+*/
+
+jest.mock('./services/DictionaryServiceImpl');
+test('Testeo mokeo del servicio', async () =>{
+    getWordMeaning.mockReturnValueOnce('Result not found, try other word');
     const responseService = await getWordMeaning('defnition');
     expect(responseService).toBe('Result not found, try other word');
 });
 
-/*
-test('Testeo mokeo del servicio', async () =>{
-
-    axios.get.mockReturnValueOnce({
-        data : [{
-            meanings:[{
-                definitions:[{
-                    definition: 'Something like such a unit of language'
-                }]
-            }]
-        }]
-    });
-
-    //Busco en la pantalla un objeto en el dom que contenga el Placeholder en cuestion, en este caso es el input para insertar la keySearch
-    const inputPlaceholder = screen.getByPlaceholderText('Inserte palabra a buscar');
-    const submitButton = screen.getByRole('button', { id:'idSubmit' });
-    
-    fireEvent.change(inputPlaceholder, {target: {value: 'word'}});
-    fireEvent.click(submitButton);
-
-    const wordmeaning = await screen.findByText(/Something like such a unit of language/i);
-
-    expect(wordmeaning).toBeInTheDocument();
-});
-*/
